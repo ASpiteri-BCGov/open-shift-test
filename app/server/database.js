dce569554f416c51ae317c8c563548e9233d58ac
poornima-sivanand <poornima.sivanand@gov.bc.ca>
2021-06-24 20:35:13 -0700

    initial commit

---
+++ app/server/database.js
@@ -0,0 +1,10 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: process.env.user,
+  password: process.env.password,
+  port:  process.env.port,
+  database: process.env.database
+});
+
+module.exports = pool;
---
+++ app/server/database.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE sampletodo;
+
+CREATE TABLE todo(
+    todo_id SERIAL PRIMARY KEY,
+    description VARCHAR(255)
+
+);
