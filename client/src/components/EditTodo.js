152882398332be6af49190f8af99c6461f7ae73e
poornima-sivanand <poornima.sivanand@gov.bc.ca>
2021-06-28 06:17:32 -0700

    add server url

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -10,7 +10,7 @@ const EditTodo = ({ todo }) => {
     try {
       const body = { description };
       const response = await fetch(
-        `http://localhost:5000/todos/${todo.todo_id}`,
+        `http://todo-server.apps.silver.devops.gov.bc.ca/todos/${todo.todo_id}`,
         {
           method: "PUT",
           headers: { "Content-Type": "application/json" },
