48c78bf4d939065906369563e01530445427f7ae
poornima-sivanand <poornima.sivanand@gov.bc.ca>
2021-08-25 09:54:53 -0700

    add ci cd

---
+++ .pipeline/lib/build.js
@@ -0,0 +1,26 @@
+'use strict';
+const {OpenShiftClientX} = require('@bcgov/pipeline-cli')
+const path = require('path');
+
+module.exports = (settings)=>{
+  const phases=settings.phases
+  const options = settings.options
+  const oc=new OpenShiftClientX(Object.assign({'namespace':phases.build.namespace}, options));
+  const phase='build'
+  var objects = []
+
+  const templatesLocalBaseUrl =oc.toFileUrl(path.resolve(__dirname, '../../openshift'))
+
+  objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/client/bc.yaml`, {
+
+  }));
+
+  objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/server/bc.yaml`, {
+
+  }));
+
+
+
+  oc.applyRecommendedLabels(objects, phases[phase].name, phase, phases[phase].changeId, phases[phase].instance)
+  oc.applyAndBuild(objects)
+}
